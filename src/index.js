88a9e3d1e1cda13176d7cb3f9b45288d9b960de4
Andy Lita <andylita@hgbhapmbad05222.fritz.box>
2021-07-09 15:16:23 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.scss';
+
+function Title (props) {
+    return (<React.Fragment>
+        <div className="Title">
+            <h1>This is the title</h1>
+        </div>
+    </React.Fragment>)
+}
+
+export default Title;
